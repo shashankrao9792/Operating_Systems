83e236869eb7c96d7851def158dd193f86a73642
Shashank Gururaj Rao <shashankrao9792@gmail.com>
2018-02-10 15:30:02 -0400

    Update user_syscall.h

--- include/user_syscall.h
+++ include/user_syscall.h
@@ -9,5 +9,5 @@ extern uint64_t syscall_args_2(uint64_t syscall, uint64_t arg_1, uint64_t arg_2)
 extern uint64_t syscall_args_3(uint64_t syscall, uint64_t arg_1, uint64_t arg_2, uint64_t arg_3);
 extern uint64_t syscall_args_4(uint64_t syscall, uint64_t arg_1, uint64_t arg_2, uint64_t arg_3, uint64_t arg_4);
 extern uint64_t syscall_args_5(uint64_t syscall, uint64_t arg_1, uint64_t arg_2, uint64_t arg_3, uint64_t arg_4, uint64_t arg_5);
-extern uint64_t _execve_ (uint64_t syscall, char* file, char** str, char** count);
+
 #endif
