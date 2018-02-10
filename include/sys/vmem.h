61fea0fa037099d7e44bbcf671cc17fee4645912
Shashank Gururaj Rao <shashankrao9792@gmail.com>
2018-02-10 15:27:54 -0400

    Update vmem.h

--- include/sys/vmem.h
+++ include/sys/vmem.h
@@ -3,28 +3,36 @@
 
 #include "sys/defs.h"
 #include "sys/process.h"
+
 //Present bit
 #define PG_P  0x1ull
+
 //Read/Write bit
 #define PG_R  0x2ull
+
 //User/Supervisor bit
 #define PG_U  0x4ull
+
 //Write Through bit
 #define PG_W  0x8ull
+
 //Cache disabled bit
 #define PG_C  0x16ull
+
 //Accessed bit
 #define PG_A  0x32ull
+
 //Dirty bit
 #define PG_D  0x64ull
+
 //Global bit
 #define PG_G  0x256ull
-#define PG_COW  1ull<<9
-//#define VMEM_START 0XFFFFFFFFF0000000ull
-#define PMEM_START 0X0ull
-uint64_t * virtual_PML4;
-//volatile int doHandlePageFault;
 
+#define PG_COW  1ull<<9
+
+#define PMEM_START 0X0ull
+
+uint64_t * virtual_PML4;
 uint64_t currentPML4;
 void walkVMA(PCB* pcb);
 void init_paging(uint64_t, uint64_t,uint64_t,uint64_t);
@@ -47,4 +55,5 @@ void userPhysicalToVirtual(uint64_t pAddress,uint64_t vAddress, uint64_t *PML4);
 void memcpy(char* addrSrc, char* addrDest, int size);
 void pageWalk(uint64_t* pml4);
 void userPhysicalToVirtual2(uint64_t pAddress,uint64_t vAddress, uint64_t *PML4);
+
 #endif
