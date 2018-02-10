b8f48517b7f29c7a489f8eafceee2afecfcb359c
Shashank Gururaj Rao <shashankrao9792@gmail.com>
2018-02-10 15:20:28 -0400

    Update defs.h

--- include/sys/defs.h
+++ include/sys/defs.h
@@ -21,48 +21,48 @@ typedef uint64_t off_t;
 
 typedef uint32_t pid_t;
 
-#define BUFFER_SIZE	200
-#define FILE_PERM	0x0777
+#define BUFFER_SIZE	  200
+#define FILE_PERM	    0x0777
 
-#define STDIN		0
+#define STDIN		  0
 #define STDOUT		1
 #define STDERR		2
 
-#define FILE_ERROR			5555
-#define FILE_ALREADY_OPEN	5556
+#define FILE_ERROR			    5555
+#define FILE_ALREADY_OPEN	  5556
 
-#define	SYS_READ 		0
+#define	SYS_READ 		  0
 #define	SYS_WRITE 		1
-#define	SYS_OPEN		2
-#define	SYS_CLOSE		3
+#define	SYS_OPEN		  2
+#define	SYS_CLOSE		  3
 #define SYS_MALLOC		9
-#define SYS_PIPE		22
-#define SYS_YIELD		24
-#define SYS_DUP2		33
-#define SYS_KILL		62
-#define SYS_SLEEP		35
+#define SYS_PIPE		  22
+#define SYS_YIELD		  24
+#define SYS_DUP2		  33
+#define SYS_KILL		  62
+#define SYS_SLEEP		  35
 
 #define SYS_GETPID		39
 
-#define	SYS_FORK 		57
-#define SYS_EXECV		59
-#define	SYS_EXIT 		60
+#define	SYS_FORK 		  57
+#define SYS_EXECV		  59
+#define	SYS_EXIT 		  60
 
-#define SYS_CAT			75
+#define SYS_CAT			  75
 
 #define SYS_GETDENTS	78
-#define SYS_CWD			79
-#define SYS_CHDIR		80
+#define SYS_CWD			  79
+#define SYS_CHDIR		  80
 #define SYS_READLINE	95
 #define SYS_CLRSCR		99
 
 #define SYS_GETPPID		110
-#define SYS_PS			270
+#define SYS_PS			  270
 #define SYS_WAITID		247
 
-#define FILE_NOT_READ 0
-#define FILE_PARTIALLY_READ 1
-#define FILE_READ_COMPLETELY 2
+#define FILE_NOT_READ         0
+#define FILE_PARTIALLY_READ   1
+#define FILE_READ_COMPLETELY  2
 
 
 #endif
