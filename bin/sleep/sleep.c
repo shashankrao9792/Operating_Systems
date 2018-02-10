4c32b4ee5e5ef5eaa453a0a4bf1f5d4291571a15
Shashank Gururaj Rao <shashankrao9792@gmail.com>
2018-02-10 15:17:46 -0400

    Update sleep.c

--- bin/sleep/sleep.c
+++ bin/sleep/sleep.c
@@ -10,20 +10,6 @@ int main(int argc, char *argV[], char *envp[])
 	char * commands = malloc(100);
 	long sleep_time_seconds = (long)atoi(argV[1]);
 	sleep(sleep_time_seconds,commands);
-//	printf("Back in string\n");
-//	printf("commands found : %s",commands);
-
-//	char* command[50];
-//
-//	int num_commands = strtok(commands, '\n', command);
-////	printf("\nnumber= %d", num_commands);
-////	char* temp = malloc(100);
-//	int i = 0;
-//	while(i<num_commands) {
-////		printf("\nHi");
-//		shebang(command[i],/* argc, argv,*/ envp);
-//		i++;
-//	}
 
 	return 0;
 }
